4e293359679524d1b3beec20740b7ba6658f5589
Łukasz Anwajler <lukasz@anwajler.com>
2010-12-30 04:30:31 +0100

    Content made editable Added device info 'share' button for easy sending

--- src/com/anwajler/sensortester/SensorTester.java
+++ src/com/anwajler/sensortester/SensorTester.java
@@ -6,24 +6,34 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.os.Build.VERSION;
+
 
 public class SensorTester extends Activity {
     Context ctx;
-    TextView content;
-
+    EditText content;
+    Button share;
+    String ver;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        ctx = getApplicationContext();
-        content = (TextView) findViewById(R.id.content);
+        ctx     = getApplicationContext();
+        content = (EditText) findViewById(R.id.content);
+        share   = (Button) findViewById(R.id.share);
+        ver     = new VERSION().RELEASE;
         
-		SensorManager sm = (SensorManager)ctx.getSystemService(SENSOR_SERVICE);
+		SensorManager sm          = (SensorManager)ctx.getSystemService(SENSOR_SERVICE);
         List <Sensor> sensorsList = (List) sm.getSensorList(Sensor.TYPE_ALL);
         ArrayList<Sensor> sensors = new ArrayList(sensorsList);
         
@@ -39,7 +49,11 @@ public class SensorTester extends Activity {
         	// API < 9, no getMinDelay method available
         }
         
-        String output = new String();
+        String output = new String(android.os.Build.MANUFACTURER + "\n" +
+        						   android.os.Build.DEVICE       + "\n" +
+        						   android.os.Build.MODEL        + "\n" +
+        						   android.os.Build.PRODUCT      + "\n" +
+        						   ver+"\n");
         if(hasMinDelay == true) {
             for(Sensor s: sensors){
             	output += 		 
@@ -66,5 +80,22 @@ public class SensorTester extends Activity {
             }        	
         }
         content.setText(output);
+        
+        // event handler
+        share.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+				shareIntent.setType("text/html");
+				shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Android Sensor Tester");
+				shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, content.getText());
+				shareIntent.setClassName("com.android.email", 
+				"com.android.email.activity.MessageCompose");				
+				startActivity(Intent.createChooser(shareIntent, "Share your device details"));				
+			}
+		});
+        
     }
 }
